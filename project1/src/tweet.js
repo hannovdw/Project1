0be9c13f4c8360605b99739d72c36fc606eb82ce
hannovdw <hannovdw1998@gmail.com>
2020-09-07 23:30:07 +0200

    changed

--- project1/src/tweet.js
+++ project1/src/tweet.js
@@ -6,7 +6,7 @@ function Tweet(){
         <div className="tweet">
             <h3>Hanno Van der walt</h3>
             <p>0823054581</p>
-            <h3>BSc.I.T.</h3>
+            <h3>BSc.I.T</h3>
         </div>
     );
 }
