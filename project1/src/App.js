ce3b77a3449b53a2470857f8c3c108a6d63a6aa4
hannovdw <hannovdw1998@gmail.com>
2020-09-08 00:33:23 +0200

    Added Image

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Tweet from "./tweet";
+import Pic from "C:\\Users\\baren\\Desktop\\Studies\\323\\Project1\\project1\\src\\pic.JPG";
 
 function App(){
   return(
     <div className="app">
       <h1>Cirriculum Vitaeeee</h1>
+      <img src = {Pic} alt="Pic" height="300" width="205" />
       <Tweet />
     </div>
   );
