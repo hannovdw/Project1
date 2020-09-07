358441598a0161020ef20deed5a3551829523996
hannovdw <hannovdw1998@gmail.com>
2020-09-08 00:54:05 +0200

    buttons and CSS added

--- project1/src/App.js
+++ project1/src/App.js
@@ -5,9 +5,13 @@ import Pic from "C:\\Users\\baren\\Desktop\\Studies\\323\\Project1\\project1\\sr
 function App(){
   return(
     <div className="app">
-      <h1>Cirriculum Vitaeeee</h1>
+      <h1 className="header">Cirriculum Vitae</h1>
       <img src = {Pic} alt="Pic" height="300" width="205" />
       <Tweet />
+      <button className="button">About Me</button>
+      <button className="button">Quilifications</button>
+      <button className="button">Hobbies</button>
+
     </div>
   );
 }
