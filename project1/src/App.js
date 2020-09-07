066155db9938226c82ee78cf00086ad4d038b056
hannovdw <hannovdw1998@gmail.com>
2020-09-07 23:27:21 +0200

    created Project

---
+++ project1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tweet from "./tweet";
+
+function App(){
+  return(
+    <div className="app">
+      <h1>Cirriculum Vitae</h1>
+      <Tweet />
+    </div>
+  );
+}
+
+export default App;
---
+++ project1/src/tweet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function Tweet(){
+    return(
+        <div className="tweet">
+            <h3>Hanno Van der walt</h3>
+            <p>0823054581</p>
+            <h3>BSc.I.T.</h3>
+        </div>
+    );
+}
+
+export default Tweet;
