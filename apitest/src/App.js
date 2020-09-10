16173f7c55d95927282ab409c5f1812c651fec46
hannovdw <hannovdw1998@gmail.com>
2020-09-10 13:43:12 +0200

    Created Joke API & improved CSS and styling

---
+++ apitest/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class FetchRandomUser extends React.Component{
+
+  state = {
+    loading: true
+  }
+
+  async componentDidMount() {
+    const url = "https://api.chucknorris.io/jokes/random";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ quote: data, loading: false});
+  }
+
+  render() {
+    return(
+       <div> 
+         {this.state.loading || !this.state.quote ? (
+          <div>loading joke...</div>
+         ) : (
+          <div>
+           <div> {this.state.quote.value} l</div>
+          </div>
+         )} 
+       </div>
+       
+    );
+  }
+}
+
--- project1/src/App.js
+++ project1/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Tweet from "./tweet";
+import Api1 from "./api";
 import Pic from "C:\\Users\\baren\\Desktop\\Studies\\323\\Project1\\project1\\src\\pic.JPG";
+
+
 
 function App(){
   return(
     <div className="app">
       <h1 className="header">Cirriculum Vitae</h1>
-      <img src = {Pic} alt="Pic" height="300" width="205" />
+      <img src = {Pic} alt="Pic" height="300" width="205" className="image" />
       <Tweet />
       <button className="button">About Me</button>
       <button className="button">Quilifications</button>
       <button className="button">Hobbies</button>
+      <Api1 />
 
     </div>
   );
