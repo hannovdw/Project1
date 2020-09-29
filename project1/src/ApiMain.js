72beac8a4bc7f75e6ca14c35bdc63ba0dc6a84b9
hannovdw <hannovdw1998@gmail.com>
2020-09-29 11:22:56 +0200

    deploy

--- project1/src/ApiMain.js
+++ project1/src/ApiMain.js
@@ -5,7 +5,7 @@ function ApiMain(){
   return(
     <div className = "paragraph">
         <h1 className = "head">API Page</h1>
-        <h3><u>Quote Of the day retrieved with REST API in JSON format: </u></h3>
+        <h3>Quote Of the day retrieved with REST API in JSON format: </h3>
         <QuoteAPI/>
 
     </div>
